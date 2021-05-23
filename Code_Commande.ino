ef85320d95d11da7d6554d4ac8b2d1689dd65eca
Noura <78438102+Blue-Nour@users.noreply.github.com>
2021-05-23 21:33:36 +0200

    test

---
+++ Code_Commande.ino
@@ -0,0 +1,70 @@
+
+#include "SerialTransfer.h" //main
+
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+SoftwareSerial mSerial( 0,1);
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+
+
+SerialTransfer myTransfer;
+
+struct STRUCT {
+  int Joy1X ;
+  int Joy1Y;
+  int Joy2X;
+  int Joy2Y;
+  bool BPBuzzer;
+  bool BPJoy1;
+  bool BPJoy2;
+  char Mode;
+  int Dist_sonar;
+  bool RFID_State;
+} testStruct;
+
+char arr[] = "hello";
+
+
+void setup()
+{
+  Serial.begin(38400);
+  mSerial.begin(38400);
+  myTransfer.begin(mSerial);
+  pinMode(21,OUTPUT);
+  digitalWrite(21,HIGH);
+}
+
+
+void loop()
+{
+  testStruct.Joy1X = analogRead(A0);  //Joy vertical
+  testStruct.Joy1Y = analogRead(A1);  //Joy horizontal
+  
+  // use this variable to keep track of how many
+  // bytes we're stuffing in the transmit buffer
+  uint16_t sendSize = 0;
+
+  ///////////////////////////////////////// Stuff buffer with struct
+  sendSize = myTransfer.txObj(testStruct, sendSize);
+
+  ///////////////////////////////////////// Stuff buffer with array
+  sendSize = myTransfer.txObj(arr, sendSize);
+
+  ///////////////////////////////////////// Send buffer
+  myTransfer.sendData(sendSize);
+
+  if(myTransfer.available())
+  {
+    // use this variable to keep track of how many
+    // bytes we've processed from the receive buffer
+    uint16_t recSize = 0;
+
+    recSize = myTransfer.rxObj(testStruct, recSize);
+  }
+  delay(500);
+}
